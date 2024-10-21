69025690eabad51a1417b575455615341909c9ff
Henry W <henry.wahhab@gmail.com>
2024-10-20 20:39:07 -0700

    state vector plot update

--- ODE solvers/ball_and_beam_part1.py
+++ ODE solvers/ball_and_beam_part1.py
@@ -5,7 +5,7 @@ import numpy as np
 g = -9.81 #gravitational constant in meters per second squared
 r = 0.2  #radius of gear in meters
 L = 1 #Length of beam in meters
-T = 0.25 #Time constant in seconds
+T = 0.25 #Delay constant in seconds
 #U is equal to 0 due to no control input so this is ignored
 
 def ball_beam(x, t):
@@ -18,10 +18,15 @@ t = np.linspace(0, 10, 1001)
 
 sol = odeint(ball_beam, x0, t, args=())
 
+plt.title(f'Initial Conditions: {x0}')
 plt.plot(t, sol[:, 0], 'b', label='z')
 plt.plot(t, sol[:, 1], 'g', label='z dot')
+plt.plot(t, sol[:, 2], 'r', label = 'theta')
+plt.plot(t, sol[:, 3], color = 'magenta', label = 'theta dot')
+
 plt.legend(loc='best')
-plt.xlabel('t')
+plt.ylabel('Units dependent on state variable')
+plt.xlabel('t (seconds)')
 plt.grid()
 plt.show()
 
